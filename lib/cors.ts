69636e3cc3c445b7b162678e6cbdd8bde79a4c61
Matthias De Mey <matthias@ocaso.be>
2025-10-22 23:57:14 +0200

    Add withCORS function to lib/cors.ts

---
+++ lib/cors.ts
@@ -0,0 +1,7 @@
+export function withCORS() {
+  return {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+  };
+}
